007fa6ca4f2e66c08883b008c227eda0601ff0cc
DevsTapePlayer <101163787+DevsTapePlayer@users.noreply.github.com>
2022-10-07 20:28:47 -0700

    Create Course.h

---
+++ Course.h
@@ -0,0 +1,35 @@
+#ifndef COURSEH
+#define COURSEH
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Course {
+	// TODO: Declare private data members - courseNumber, courseTitle
+   private:
+	   string courseNumber;
+	   string courseTitle;
+
+
+	// TODO: Declare mutator functions - 
+	//       SetCourseNumber(), SetCourseTitle()
+   public: 
+	   void SetCourseNumber(string newcourseNumber);
+ 	   void SetCourseTitle(string newcourseTitle);
+
+
+	// TODO: Declare accessor functions - 
+	//       GetCourseNumber(), GetCourseTitle()
+	   string GetCourseNumber();
+	   string GetCourseTitle();
+
+
+
+	// TODO: Declare PrintInfo()
+	   void PrintInfo();
+
+};
+
+#endif
